985ca13f0e7344466ed7b143f996131f8867ef3d
Jin-Mach <ji82ma@seznam.cz>
2025-07-05 15:26:50 +0200

    Created ProgressDialog class with progress bar and localized text for advanced search, used in AdvancedSearchController.

--- src/contacts/controlers/advanced_search_controler.py
+++ src/contacts/controlers/advanced_search_controler.py
@@ -6,6 +6,7 @@ from src.contacts.threading.objects.advanced_search_object import AdvancedSearch
 from src.contacts.threading.objects.user_filter_object import UserFilterObject
 from src.contacts.ui.main_widgets.contacts_statusbar_widget import ContactsStatusbarWidget
 from src.contacts.ui.search_dialogs.advanced_search_dialog import AdvancedSearchDialog
+from src.contacts.ui.shared_widgets.progress_dialog import ProgressDialog
 from src.database.models.mandatory_model import MandatoryModel
 from src.database.utilities.query_provider import QueryProvider
 from src.database.utilities.search_provider import SearchProvider
@@ -25,11 +26,13 @@ class AdvancedSearchController:
         self.parent = parent
         self.dialog = AdvancedSearchDialog(self.parent)
         self.query_provider = QueryProvider()
+        self.progress_dialog = ProgressDialog(self.parent)
 
     def advanced_search(self) -> None:
         try:
             self.dialog.reset_all_filters()
             if self.dialog.exec() == QDialog.DialogCode.Accepted:
+                self.progress_dialog.show_dialog()
                 filters = self.dialog.get_final_filter()
                 self.advanced_search_object = AdvancedSearchObject(self.db_connection.databaseName(), self.query_provider, filters, self.parent)
                 self.advanced_search_thread = BasicThread()
@@ -38,12 +41,13 @@ class AdvancedSearchController:
                                                              success_signal=self.advanced_search_object.search_completed,
                                                              success_callback=self.check_search_result,
                                                              on_error=self.log_and_show_error,
-                                                             on_finished=lambda: AdvancedSearchController.remove_connection(self.advanced_search_object.connection_name))
+                                                             on_finished=lambda: self.search_finished(self.advanced_search_object.connection_name))
         except Exception as e:
             ErrorHandler.exception_handler(e, self.parent)
 
     def apply_saved_filter(self, selected_filter: dict) -> None:
         try:
+            self.progress_dialog.show_dialog()
             self.user_filter_object = UserFilterObject(self.db_connection.databaseName(), self.query_provider, selected_filter, self.parent)
             self.user_filter_thread = BasicThread()
             self.user_filter_thread.run_basic_thread(worker=self.user_filter_object,
@@ -51,7 +55,7 @@ class AdvancedSearchController:
                                                      success_signal=self.user_filter_object.search_completed,
                                                      success_callback=self.check_search_result,
                                                      on_error=self.log_and_show_error,
-                                                     on_finished=lambda: AdvancedSearchController.remove_connection(self.user_filter_object.connection_name))
+                                                     on_finished=lambda: self.search_finished(self.user_filter_object.connection_name))
         except Exception as e:
             ErrorHandler.exception_handler(e, self.parent)
 
@@ -64,9 +68,9 @@ class AdvancedSearchController:
         self.mandatory_model.set_filter_by_id(id_list)
         self.contacts_statusbar.set_count_text(self.mandatory_model.rowCount(), 0)
 
-    @staticmethod
-    def remove_connection(connection_name: str)-> None:
+    def search_finished(self, connection_name: str)-> None:
         QSqlDatabase.removeDatabase(connection_name)
+        self.progress_dialog.hide_dialog()
 
     @staticmethod
     def log_and_show_error(error: str) -> None:
---
+++ src/contacts/ui/shared_widgets/progress_dialog.py
@@ -0,0 +1,47 @@
+from PyQt6.QtCore import Qt
+
+from PyQt6.QtWidgets import QDialog, QLayout, QVBoxLayout, QLabel, QProgressBar
+
+from src.utilities.error_handler import ErrorHandler
+from src.utilities.language_provider import LanguageProvider
+
+
+class ProgressDialog(QDialog):
+    def __init__(self, parent=None) -> None:
+        super().__init__(parent)
+        self.setObjectName("progressDialog")
+        self.setFixedSize(250, 70)
+        self.setWindowFlag(Qt.WindowType.FramelessWindowHint |Qt.WindowType.Dialog)
+        self.setModal(True)
+        self.setLayout(self.create_gui())
+        self.set_ui_text()
+
+    def create_gui(self) -> QLayout:
+        main_layout = QVBoxLayout()
+        self.search_text_label = QLabel()
+        self.search_text_label.setObjectName("searchTextLabel")
+        self.search_text_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.progress_bar = QProgressBar(self)
+        self.progress_bar.setFixedHeight(15)
+        self.progress_bar.setRange(0, 0)
+        main_layout.addWidget(self.search_text_label)
+        main_layout.addWidget(self.progress_bar)
+        return main_layout
+
+    def set_ui_text(self) -> None:
+        try:
+            ui_text = LanguageProvider.get_search_dialog_text(self.objectName())
+            widgets = self.findChildren(QLabel)
+            for widget in widgets:
+                if widget.objectName() in ui_text:
+                    widget.setText(ui_text[widget.objectName()])
+        except Exception as e:
+            ErrorHandler.exception_handler(e, self)
+
+    def show_dialog(self) -> None:
+        if not self.isVisible():
+            self.show()
+
+    def hide_dialog(self) -> None:
+        if self.isVisible():
+            self.deleteLater()
